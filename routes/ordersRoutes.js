c584f4bcf7aa4f2e717a2583c814ce16db957117
IsurikaDaniel <d.nethmiisurika@gmail.com>
2025-05-24 01:39:38 +0530

    get API implementations

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -13,4 +13,29 @@ router.post('/addOrder', async (req , res ) => {
     }
 });
 
+// GET orders by product name
+router.get('/getOrder/:product', async (req, res) => {
+    try {
+        const product = req.params.product;
+        console.log('Searching for:', product);
+
+        const ordersList = await Orders.find({
+            product: { $regex: new RegExp(product, 'i') }
+        });
+
+        console.log('Found orders:', ordersList);
+
+        if (!ordersList || ordersList.length === 0) {
+            return res.status(404).json({ message: 'No orders found for this product' });
+        }
+
+        res.status(200).json(ordersList);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+
 module.exports = router;
